8c60c8fb69cb4b2fca46174a9588570023f1a9b4
codingChewie <matthewtimothy910@yahoo.com>
2019-11-05 11:24:34 -0600

    HTTP Status Codes

--- app.js
+++ app.js
@@ -20,22 +20,27 @@ app.get('/quotes', async (req, res) => {
 app.get('/quotes/:id', async (req, res) => {
   try {
     const quote = await records.getQuote(req.params.id)
-    return res.json(quote)
+    if (quote) return res.json({ message: 'Not found' })
+
+    res.status(404).json()
   } catch (err) {
-    return res.json({ message: err.message })
+    return res.status(500).json({ message: err.message })
   }
 })
 
 // Send a POST request to /quotes to  CREATE a new quote
 app.post('/quotes', async (req, res) => {
   try {
-    const quote = await records.createQuote({
-      quote: req.body.quote,
-      author: req.body.author,
-    })
-    return res.json(quote)
+    if (req.body.author && req.body.quote) {
+      const quote = await records.createQuote({
+        quote: req.body.quote,
+        author: req.body.author,
+      })
+      return res.status(201).json(quote)
+    }
+    return res.status(400).json({ message: 'Quote and author required' })
   } catch (err) {
-    return res.json({ message: err.message })
+    return res.stsatus(500).json({ message: err.message })
   }
 })
 
