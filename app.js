bfd79ace3b53f663065ac325ed4a8b69e928fc6b
codingChewie <matthewtimothy910@yahoo.com>
2019-11-05 10:46:21 -0600

    Async/Await in Express

--- app.js
+++ app.js
@@ -5,19 +5,12 @@ const app = express()
 const records = require('./records')
 
 // Send a GET request to /quotes to READ a list of quotes
-app.get('/quotes', (req, res) => {
-  const quotes = records.getQuotes()
+app.get('/quotes', async (req, res) => {
+  const quotes = await records.getQuotes()
   res.json(quotes)
 })
 
 // Send a GET request to /quotes/:id to READ(view) a quote
-// app.get('/quotes/:id', (req, res) => {
-//   console.log(req.params.id)
-
-//   const quote = data.records.find((quote) => quote.id === req.params.id)
-//   res.json(quote)
-// })
-
 app.get('/quotes/:id', (req, res) => {})
 
 // Send a POST request to /quotes to  CREATE a new quote
