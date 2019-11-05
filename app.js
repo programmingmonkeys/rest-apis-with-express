577c62b716782fbdb6e87d01ca336501f5ad19e5
codingChewie <matthewtimothy910@yahoo.com>
2019-11-04 22:07:03 -0600

    GET a Quote, GET all Quotes

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express')
+const data = require('./data')
 
 const app = express()
 
-app.get('/greetings', (req, res) => {
-  res.json({ greetings: 'monday blues' })
+// Send a GET request to /quotes to READ a list of quotes
+app.get('/quotes', (req, res) => {
+  res.json(data)
 })
 
-// Send a GET request to /quotes to READ a list of quotes
 // Send a GET request to /quotes/:id to READ(view) a quote
+app.get('/quotes/:id', (req, res) => {
+  console.log(req.params.id)
+
+  const quote = data.records.find((quote) => quote.id == req.params.id)
+  res.json(quote)
+})
+
 // Send a POST request to /quotes to  CREATE a new quote
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote
 // Send a DELETE request to /quotes/:id DELETE a quote
