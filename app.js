29cece2cb50e9bcb560f1457387fe5822325f365
codingChewie <matthewtimothy910@yahoo.com>
2019-11-05 11:10:51 -0600

    Using Try/Catch with Async/Await

--- app.js
+++ app.js
@@ -8,23 +8,35 @@ app.use(express.json())
 
 // Send a GET request to /quotes to READ a list of quotes
 app.get('/quotes', async (req, res) => {
-  const quotes = await records.getQuotes()
-  res.json(quotes)
+  try {
+    const quotes = await records.getQuotes()
+    return res.json(quotes)
+  } catch (err) {
+    return res.json({ message: err.message })
+  }
 })
 
 // Send a GET request to /quotes/:id to READ(view) a quote
 app.get('/quotes/:id', async (req, res) => {
-  const quote = await records.getQuote(req.params.id)
-  res.json(quote)
+  try {
+    const quote = await records.getQuote(req.params.id)
+    return res.json(quote)
+  } catch (err) {
+    return res.json({ message: err.message })
+  }
 })
 
 // Send a POST request to /quotes to  CREATE a new quote
 app.post('/quotes', async (req, res) => {
-  const quote = await records.createQuote({
-    quote: req.body.quote,
-    author: req.body.author,
-  })
-  res.json(quote)
+  try {
+    const quote = await records.createQuote({
+      quote: req.body.quote,
+      author: req.body.author,
+    })
+    return res.json(quote)
+  } catch (err) {
+    return res.json({ message: err.message })
+  }
 })
 
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote
