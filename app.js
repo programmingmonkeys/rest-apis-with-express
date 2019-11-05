a288a2131ea2b3abd4b4f911d0326e8c87c04204
codingChewie <matthewtimothy910@yahoo.com>
2019-11-05 11:58:56 -0600

    Refactor with Express Middleware

--- app.js
+++ app.js
@@ -4,33 +4,42 @@ const app = express()
 
 const records = require('./records')
 
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
 app.use(express.json())
 
 // Send a GET request to /quotes to READ a list of quotes
-app.get('/quotes', async (req, res) => {
-  try {
+app.get(
+  '/quotes',
+  asyncHandler(async (req, res) => {
     const quotes = await records.getQuotes()
     return res.json(quotes)
-  } catch (err) {
-    return res.json({ message: err.message })
-  }
-})
+  }),
+)
 
 // Send a GET request to /quotes/:id to READ(view) a quote
-app.get('/quotes/:id', async (req, res) => {
-  try {
+app.get(
+  '/quotes/:id',
+  asyncHandler(async (req, res) => {
     const quote = await records.getQuote(req.params.id)
     if (quote) return res.json({ message: 'Not found' })
 
     res.status(404).json()
-  } catch (err) {
-    return res.status(500).json({ message: err.message })
-  }
-})
+  }),
+)
 
 // Send a POST request to /quotes to  CREATE a new quote
-app.post('/quotes', async (req, res) => {
-  try {
+app.post(
+  '/quote',
+  asyncHandler(async (req, res) => {
     if (req.body.author && req.body.quote) {
       const quote = await records.createQuote({
         quote: req.body.quote,
@@ -39,14 +48,13 @@ app.post('/quotes', async (req, res) => {
       return res.status(201).json(quote)
     }
     return res.status(400).json({ message: 'Quote and author required' })
-  } catch (err) {
-    return res.stsatus(500).json({ message: err.message })
-  }
-})
+  }),
+)
 
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote
-app.put('/quotes/:id', async (req, res) => {
-  try {
+app.put(
+  '/quotes/:id',
+  asyncHandler(async (req, res) => {
     const quote = await records.getQuote(req.params.id)
     if (quote) {
       quote.quote = req.body.quote
@@ -56,22 +64,19 @@ app.put('/quotes/:id', async (req, res) => {
       res.status(204).end()
     }
     return res.status(404).json({ message: 'Quote Not Found' })
-  } catch (err) {
-    return res.status(500).json({ message: err.message })
-  }
-})
+  }),
+)
 
 // Send a DELETE request to /quotes/:id DELETE a quote
-app.delete('/quotes/:id', async (req, res, next) => {
-  try {
+app.delete(
+  '/quotes/:id',
+  asyncHandler(async (req, res, next) => {
     const quote = await records.getQuote(req.params.id)
     await records.deleteQuote(quote)
 
     res.status(204).end()
-  } catch (err) {
-    next(err)
-  }
-})
+  }),
+)
 
 // Send a GET request to /quotes/quote/random to READ (view) a random quote
 
