25a0cb4d1a9e8dca111f00f2c512c8437fb996f7
codingChewie <matthewtimothy910@yahoo.com>
2019-11-04 21:52:16 -0600

    Planning out a REST API

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ app.get('/greetings', (req, res) => {
   res.json({ greetings: 'monday blues' })
 })
 
+// Send a GET request to /quotes to READ a list of quotes
+// Send a GET request to /quotes/:id to READ(view) a quote
+// Send a POST request to /quotes to  CREATE a new quote
+// Send a PUT request to /quotes/:id to UPDATE (edit) a quote
+// Send a DELETE request to /quotes/:id DELETE a quote
+// Send a GET request to /quotes/quote/random to READ (view) a random quote
+
 app.listen(3000, () => console.log('Quote API listening on port 3000!'))
